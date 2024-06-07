942a60b760e68eb9e06ecf3662022229ab1bfc40
gikhoon <ghikhoon@naver.com>
2024-06-07 18:47:41 +0900

    feat: 11726 완료

---
+++ src/BaekJoon11726/Main.java
@@ -0,0 +1,27 @@
+package BaekJoon11726;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+     static int[] list;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        list = new int[n+2];
+        list[1] = 1;
+        list[2] = 2;
+
+        for (int i = 3; i < n + 1; i++) {
+            list[i] = (list[i - 1] + list[i - 2]) % 10007;
+        }
+
+        System.out.println(list[n]);
+    }
+}
