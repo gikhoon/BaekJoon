fa1baef824b27eb601805df4bae6b4e3e753e54e
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2024-06-20 01:30:02 +0900

    [Silver II] Title: DFS와 BFS, Time: 352 ms, Memory: 22208 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static boolean[][] graph;
+    public static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        graph = new boolean[N + 1][N + 1];
+
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            graph[x][y] = true;
+            graph[y][x] = true;
+        }
+
+        DFS(V);
+        BFS(V);
+    }
+
+    static void DFS(int v) {
+        StringBuilder sb = new StringBuilder();
+        boolean[] isUsed = new boolean[N + 1];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(v);
+
+        while (!stack.isEmpty()) {
+            Integer node = stack.peek();
+            if (!isUsed[node]) {
+                sb.append(node).append(" ");
+                isUsed[node] = true;
+            }
+            boolean isConnected = false;
+            for (int i = 1; i < N + 1; i++) {
+                if (!isUsed[i] && graph[node][i]) {
+                    stack.push(i);
+                    isConnected = true;
+                    break;
+                }
+            }
+            if (!isConnected) {
+                stack.pop();
+            }
+        }
+        System.out.println(sb);
+    }
+
+    static void BFS(int v) {
+        StringBuilder sb = new StringBuilder();
+        boolean[] isUsed = new boolean[N + 1];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(v);
+
+        while (!queue.isEmpty()) {
+            Integer node = queue.poll();
+            if (!isUsed[node]) {
+                sb.append(node).append(" ");
+                isUsed[node] = true;
+            }
+            for (int i = 1; i < N + 1; i++) {
+                if (!isUsed[i] && graph[node][i]) {
+                    queue.offer(i);
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
