74d7ba2148b9aa608557d618871b3fda23cb959a
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2024-07-01 15:35:42 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 484 ms, Memory: 45416 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,39 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static int count = 0;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        hanoi(N, 1, 3);
+
+        System.out.println(count);
+        System.out.println(sb);
+    }
+
+    private static void hanoi(int n, int start, int end) {
+        if (n == 1) {
+            count++;
+            sb.append(start).append(" ").append(end).append("\n");
+        } else {
+            hanoi(n-1,start,findEmpty(start,end));
+            sb.append(start).append(" ").append(end).append("\n");
+            count++;
+            hanoi(n-1,findEmpty(start,end),end);
+        }
+    }
+
+    private static int findEmpty(int start, int end) {
+        for (int i = 1; i <= 3; i++) {
+            if (i != start && i != end) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
