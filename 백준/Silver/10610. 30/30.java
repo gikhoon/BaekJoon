ea0f34e98bdbf892c8eb885cc4c05e02d88fea90
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2024-07-02 17:34:10 +0900

    [Silver IV] Title: 30, Time: 348 ms, Memory: 24952 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -9,28 +9,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] split = br.readLine().split("");
         Arrays.sort(split);
-
-        int[] numList = new int[split.length];
+        
         if (Integer.parseInt(split[0]) != 0) {
             System.out.println(-1);
             return;
         }
-        numList[0] = 0;
+        
         int sum = 0;
-        for (int i = 1; i < split.length; i++) {
-            numList[i] = Integer.parseInt(split[i]);
-            sum += numList[i];
+        StringBuilder sb = new StringBuilder();
+        for (int i = split.length -1; i >= 0; i--) {
+            sum += Integer.parseInt(split[i]);
+            sb.append(split[i]);
         }
 
         if (sum % 3 != 0) {
             System.out.println(-1);
             return;
-        }
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = numList.length-1; i >= 0; i--) {
-            sb.append(numList[i]);
         }
 
         System.out.println(sb);
