d4eea8d81b9b103a35e081e1a8615bd9312dcc5e
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2024-07-16 18:54:55 +0900

    [Gold IV] Title: 부분합, Time: 288 ms, Memory: 23636 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -6,48 +6,41 @@ public class Main {
 
     static int[] list;
 
-    static int M,N;
+    static int S,N;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
         list = new int[N];
 
         st = new StringTokenizer(br.readLine());
         int answer = Integer.MAX_VALUE;
-        list[0] = Integer.parseInt(st.nextToken());
-        if (list[0] == M) {
-            System.out.println(1);
-            return;
-        }
-        for (int i = 1; i < N; i++) {
-            list[i] = Integer.parseInt(st.nextToken()) + list[i-1];
-            if (list[i] >= M) {
-                answer = Math.min(answer, i + 1);
+        int sum = 0;
+        int start = 0; int len = 0;
+        for (int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+            sum += list[i];
+            if (sum >= S) {
+                while (sum - list[start] >= S && start < i) {
+                    sum -= list[start++];
+                }
+                len = i - start + 1;
+                if (len == 1) {
+                    System.out.println(1);
+                    return;
+                }
+                if (len < answer) {
+                    answer = len;
+                }
+//                sum -= list[start++];
             }
         }
 
-        if (Integer.MAX_VALUE == answer) {
-            System.out.println(0);
-            return;
-        }
-
-        for (int i = 1; i < N; i++) {
-            for (int j = i; j < N; j++) {
-                if (j - i + 1 >= answer) {
-                    break;
-                }
-                if (list[j] - list[i - 1] >= M) {
-                    answer = Math.min(answer, j - i + 1);
-                    break;
-                }
-            }
-        }
-
-        System.out.println(answer);
+        if (answer == Integer.MAX_VALUE) System.out.println(0);
+        else System.out.println(answer);
     }
 }
 
