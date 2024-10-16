ecbd071aab1f4f52fc748fffcde98ea09fb6692d
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2024-10-16 13:16:26 +0900

    [level 2] Title: 귤 고르기, Time: 98.65 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,33 +1,25 @@
 import java.util.*;
+
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
-        Arrays.sort(tangerine);
-        List<Integer> countList = new ArrayList<>();
-        
-        int index = 0;
-        while(index < tangerine.length){
-            int count = 1;
-            int data = tangerine[index];
-            index++;
-            
-            while(index < tangerine.length){
-                if(tangerine[index] != data) break;
-                count++;
-                index++;
-            }
-            
-            countList.add(count);
+        HashMap<Integer,Integer> map =new HashMap<>();
+
+        for (int t : tangerine) {
+            map.put(t, map.getOrDefault(t, 0) + 1);
         }
-        
-        Collections.sort(countList, Comparator.reverseOrder());
-        
-        for(int count : countList){
-            if(k <= 0) break;
+
+        List<Integer> list = new ArrayList<>(map.keySet());
+        list.sort((o1, o2) -> map.get(o2)-map.get(o1));
+
+        for(Integer key:list){
+            k -=map.get(key);
             answer++;
-            k-=count;
+            if(k<=0){
+                break;
+            }
         }
-        
+
         return answer;
     }
 }
