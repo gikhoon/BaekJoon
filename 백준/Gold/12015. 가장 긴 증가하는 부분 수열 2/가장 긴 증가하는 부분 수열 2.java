5383c6731caf3141ef09bf2268a183c6c2ba19fe
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-04-22 11:30:18 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 472 ms, Memory: 92208 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,42 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
         int[] list = new int[N];
         int lastIndex = -1;
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         for (int i = 0; i < N; i++) {
-            int x = Integer.parseInt(st.nextToken());
-
-            if (lastIndex < 0 || list[lastIndex] < x) {
+            int n = Integer.parseInt(st.nextToken());
+            if (lastIndex < 0 || list[lastIndex] < n) {
                 lastIndex++;
-                list[lastIndex] = x;
-            } else {
-                int index = findIndex(0, lastIndex, x, list);
-                list[index] = x;
+                list[lastIndex] = n;
+                continue;
             }
+
+            int index = findIndex(lastIndex, n, list);
+            list[index] = n;
         }
 
-        System.out.println(lastIndex + 1);
+        System.out.println(lastIndex+1);
     }
 
-    private static int findIndex(int left, int right, int x, int[] list) {
+    private static int findIndex(int lastIndex, int num, int[] list) {
+        int left = 0;
+        int right = lastIndex;
         while (left < right) {
             int mid = (left + right) / 2;
-            if (list[mid] < x) left = mid + 1;
+            if (list[mid] < num) left = mid + 1;
             else right = mid;
         }
-
         return left;
     }
 }
