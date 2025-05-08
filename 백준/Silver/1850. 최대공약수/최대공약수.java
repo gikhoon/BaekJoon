8b6468b02721355b0e9a151ec98d09753f98eadd
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-05-08 10:25:33 +0900

    [Silver I] Title: 최대공약수, Time: 276 ms, Memory: 38688 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long size = gcd(A, B);
+        StringBuilder sb = new StringBuilder();
+        for (long i = 0; i < size; i++) {
+            sb.append("1");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static long gcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
