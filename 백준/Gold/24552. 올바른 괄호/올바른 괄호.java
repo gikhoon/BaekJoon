560f5e21272ada9404094e3149f84e43141bd70f
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-08-08 11:10:04 +0900

    [Gold IV] Title: 올바른 괄호, Time: 144 ms, Memory: 15804 KB -BaekjoonHub

---
+++ 백준/Gold/24552. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,51 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+
+		int left = 0, right = 0;
+		for(int i=0;i<s.length();i++) {
+			if(s.charAt(i) == '(') {
+				left++;
+			} else {
+				right++;
+			}
+		}
+
+		if (left > right) {
+			left = 0;
+			right = 0;
+			for(int i=0;i<s.length();i++) {
+				if(s.charAt(i) == '(') {
+					left++;
+				} else {
+					right++;
+				}
+				if (left == right) {
+					left = 0;
+					right = 0;
+				}
+			}
+
+			System.out.println(left);
+		} else {
+			left = 0;
+			right = 0;
+			for(int i=s.length()-1;i>=0;i--) {
+				if(s.charAt(i) == ')') {
+					right++;
+				} else {
+					left++;
+				}
+				if (left == right) {
+					left = 0;
+					right = 0;
+				}
+			}
+
+			System.out.println(right);
+		}
+	}
+}
