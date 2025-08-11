9f4878cf8459d6dd583934c761f8c9178a517ad3
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-08-11 13:39:08 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 552 ms, Memory: 142100 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,62 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static boolean[][] graph;
-    public static int N;
-    public static boolean[] isUsed;
-
-    public static void main(String[] args) throws IOException {
+    static ArrayList<Integer>[] link;
+    static boolean[] visited;
+	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
-        graph = new boolean[N + 1][N + 1];
-        isUsed = new boolean[N + 1];
+        link = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+        for (int i = 1; i <= N; i++) {
+            link[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            graph[x][y] = true;
-            graph[y][x] = true;
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            link[u].add(v);
+            link[v].add(u);
         }
-        int connectedComponent = 0;
+
+        int server=0;
         for (int i = 1; i <= N; i++) {
-            if (!isUsed[i]) {
-                connectedComponent++;
-                DFS(i);
+            if (!visited[i]) {
+                server++;
+                bfs(i);
             }
         }
 
-        System.out.println(connectedComponent);
-    }
+        System.out.println(server);
+	}
 
-    static void DFS(int v) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(v);
-
-        while (!stack.isEmpty()) {
-            Integer node = stack.peek();
-            if (!isUsed[node]) {
-                isUsed[node] = true;
-            }
-            boolean isConnected = false;
-            for (int i = 1; i < N + 1; i++) {
-                if (!isUsed[i] && graph[node][i]) {
-                    stack.push(i);
-                    isConnected = true;
-                    break;
+    static void bfs(int start) {
+        visited[start] = true;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        while (!q.isEmpty()) {
+            for (int next : link[q.poll()]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    q.add(next);
                 }
             }
-            if (!isConnected) {
-                stack.pop();
-            }
         }
     }
+
 }
