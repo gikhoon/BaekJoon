5a8bd4a1c289aeea94fee7d9e24a67b52a39cfdf
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-08-13 12:13:46 +0900

    [Gold II] Title: 빵집, Time: 1392 ms, Memory: 323320 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] dr = {-1, 0, 1};
+	static int N, M;
+	static boolean[][] map;
+	static boolean[][] visited;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new boolean[N][M];
+		visited = new boolean[N][M];
+
+		for (int i = 0; i < N; i++) {
+			String[] split = br.readLine().split("");
+			for (int j = 0; j < M; j++) {
+				if (split[j].equals(".")) {
+					map[i][j] = true;
+				}
+			}
+		}
+
+		int sum = 0;
+		for (int r = 0; r < N; r++) {
+			if (dfs(r, 0)) {
+				sum++;
+			}
+		}
+
+		System.out.println(sum);
+	}
+
+	static boolean dfs(int r, int c) {
+		if (c == M - 1) {
+			map[r][c] = false;
+			return true;
+		}
+
+		visited[r][c] = true;
+
+		boolean isOk = false;
+		for (int d = 0; d < 3; d++) {
+			if (r + dr[d] < 0 || r + dr[d] >= N)
+				continue;
+			if (visited[r + dr[d]][c + 1]) {
+				continue;
+			}
+			if (map[r + dr[d]][c + 1]) {
+				isOk = dfs(r + dr[d], c + 1);
+			}
+			if (isOk) {
+				map[r][c] = false;
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
