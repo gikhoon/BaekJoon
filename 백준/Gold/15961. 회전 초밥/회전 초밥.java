31fa735a1c9fbd556ca90d6e17541f867fccfb6b
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-09-09 15:04:15 +0900

    [Gold IV] Title: 회전 초밥, Time: 548 ms, Memory: 170920 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -4,48 +4,59 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] dish;
+    static int[] count;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); // 접시 수
-        int d = Integer.parseInt(st.nextToken()); // 초밥 가짓수
-        int k = Integer.parseInt(st.nextToken()); // 연속해서 먹는 접시 수
-        int c = Integer.parseInt(st.nextToken()); // 쿠폰 번호
+        int N = Integer.parseInt(st.nextToken()); //접시 수
+        int d = Integer.parseInt(st.nextToken()); //초밥 종류 수
+        int k = Integer.parseInt(st.nextToken()); //연속해서 먹은 접시 수
+        int c = Integer.parseInt(st.nextToken()); //쿠폰 번호
 
-        int[] dish = new int[N];
+        dish = new int[N];
+        count = new int[d + 1];
+        int total = 0;
+        int max = 0;
+
         for (int i = 0; i < N; i++) {
             dish[i] = Integer.parseInt(br.readLine());
         }
 
-        int[] eatSushi = new int[d + 1]; // 초밥 종류별 먹은 횟수
-        int total = 0; // 현재 먹은 초밥의 가짓수
-        int max = 0; // 최댓값 저장
-        
         for (int i = 0; i < k; i++) {
-            if (eatSushi[dish[i]] == 0) {
+            if (count[dish[i]] == 0) {
                 total++;
             }
-            eatSushi[dish[i]]++;
+            count[dish[i]]++;
         }
-        
-        max = (eatSushi[c] == 0) ? total + 1 : total;
-        
+
+        if (count[c] == 0) {
+            max = total + 1;
+        } else {
+            max = total;
+        }
+
+        //0 ~ 5 k=6 6을 더하고 0을 빼
         for (int start = 0; start < N; start++) {
-            int end = (start + k) % N; // 회전 고려하여 끝 점 계산
-            
-            if (eatSushi[dish[start]] == 1) {
+            int end = (start + k) % N;
+
+            if (count[dish[start]] == 1) {
                 total--;
             }
-            eatSushi[dish[start]]--;
-            
-            if (eatSushi[dish[end]] == 0) {
+            count[dish[start]]--;
+
+            if (count[dish[end]] == 0) {
                 total++;
             }
-            eatSushi[dish[end]]++;
-            
-            int currentMax = (eatSushi[c] == 0) ? total + 1 : total;
-            max = Math.max(max, currentMax);
+            count[dish[end]]++;
+
+            if (count[c] == 0) {
+                max = Math.max(max, total + 1);
+            } else {
+                max = Math.max(max, total);
+            }
         }
 
         System.out.println(max);
