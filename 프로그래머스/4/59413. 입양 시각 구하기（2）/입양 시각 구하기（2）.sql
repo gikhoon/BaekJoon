fd8a5ac0eb845de16ff319864c2fda6c468983ed
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-11-15 09:26:00 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,17 +1,26 @@
-With recursive outs as (
-    SELECT 0 as N
+WITH outs (n) AS (
+    -- 0부터 시작
+    SELECT 0
+    FROM dual
     
     UNION ALL
     
-    SELECT N+1 from outs
-    where N < 23
+    -- 23까지 증가
+    SELECT n + 1
+    FROM outs
+    WHERE n < 23
+),
+agg AS (
+    SELECT 
+        TO_NUMBER(TO_CHAR(a.datetime, 'HH24')) AS hour,
+        COUNT(*) AS cnt
+    FROM ANIMAL_OUTS a
+    GROUP BY TO_NUMBER(TO_CHAR(a.datetime, 'HH24'))
 )
-
-
-SELECT N as HOUR, COUNT(ANIMAL_ID) as COUNT
-FROM OUTS o left join ANIMAL_OUTS a on o.N = HOUR(a.DATETIME)
-GROUP BY N
-order by N
-
-# SELECT *
-# FROM OUTS o left join ANIMAL_OUTS a on o.N = HOUR(a.DATETIME)
+SELECT 
+    o.n AS hour,
+    NVL(a.cnt, 0) AS count
+FROM outs o
+LEFT JOIN agg a 
+    ON a.hour = o.n
+ORDER BY o.n;
